e7a335cb8ba9acb53b27d1727202bfe870992871
atharvakaplay123 <atharvakaplay@gmail.com>
2026-04-04 02:27:59 +0530

    backend: cors updated

--- smartflow-backend/dist/app.js
+++ smartflow-backend/dist/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {} from "express";
 import "dotenv/config";
 import controller from './router/controller.js';
 import cookieParser from "cookie-parser";
@@ -8,6 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.get('/', (req, res) => { res.status(200).send("server working"); });
 app.use('/api', controller);
 app.listen(PORT, () => { console.log('server started'); });
 //# sourceMappingURL=app.js.map
---
+++ smartflow-backend/src/services/greenTimeLogic.ts
@@ -0,0 +1,5 @@
+// import type { trafficStateOfSquare } from "../types.js";
+
+// export async function greenTimeLogic(vehicleCount: trafficStateOfSquare):Promise<trafficStateOfSquare>{
+    
+// }
