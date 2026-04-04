1896fcc7620c7d13d579b28295e8840394a7a9a4
atharvakaplay123 <atharvakaplay@gmail.com>
2026-04-04 11:05:32 +0530

    backend: types for simulation api added

--- smartflow-backend/src/types.ts
+++ smartflow-backend/src/types.ts
@@ -8,3 +8,15 @@ export type trafficStateOfSquare = {
 export type trafficStateOfRoute = {
     vehicles: number
 }
+
+// body structures for simulation APIs :-
+
+export type vehicleCountABC = {
+    A: number,
+    B: number,
+    C: number
+}
+
+export type vehicleCountD = {
+    D: number
+}
