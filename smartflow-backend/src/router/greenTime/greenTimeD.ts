e53b63d487d0f686752224d4fb8ae4fea3770dd7
atharvakaplay123 <atharvakaplay@gmail.com>
2026-04-04 11:55:27 +0530

    green time logic routes created, currently in development

---
+++ smartflow-backend/src/router/greenTime/greenTimeD.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+const router = express.Router()
+router.use(express.json())
+
+router.post('/', async (req: Request, res: Response) => {
+    try {
+
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            message: 'Internal Server Error'
+        })
+    }
+
+})
+
+export default router
